f7cee66496231e034d757a9bed90ed9cacbe2cf9
Tish254 <tiegomseeraoscar295@gmail.com>
2022-04-02 16:04:23 +0300

    akan-name

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function changeSection(elemFrom) {
+
+    document.querySelector('.display-sec').classList.remove('display-sec');
+
+    if (elemFrom == 'generate-home') {
+
+        console.log(elemFrom);
+        document.getElementById('generate').classList.add('display-sec');
+        
+
+    } else if  (elemFrom == 'learn-home') {
+        
+        console.log(elemFrom);
+        document.getElementById("more").classList.add('display-sec');
+
+    }
+}
